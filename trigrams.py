0391f2e1ba354e0d0f7f0a646cd4d8b6795dfff1
Brendan Davis <brendanmd@gmail.com>
2017-10-19 14:06:07 -0700

    reads a file and creates a dictionary with list values

--- trigrams.py
+++ trigrams.py
@@ -1,9 +1,6 @@
-
-
 my_string = """It was the best of times. It was the worst of times."""
 
 the_sequence = {}
-new_string = my_string.split()
 
 
 def read_file(text_file, result_file=None):
@@ -13,16 +10,19 @@ def read_file(text_file, result_file=None):
     the_file.close()
     return data
 
-for index, item in enumerate(new_string):
-    if index < len(new_string) - 2:
-        dic_key = new_string[index] + " " + new_string[index + 1]
-        if dic_key in the_sequence:
-            the_sequence[dic_key].append(new_string[index + 2])
-            print("This is a duplicate item")
-        else:
-            the_sequence[dic_key] = [new_string[index + 2]]
-for item in the_sequence:
-    print(item + " ---- " + str(the_sequence[item]))
 
+def create_dict(file_name):
+    new_string = file_name.split()
+    for index, item in enumerate(new_string):
+        if index < len(new_string) - 2:
+            dic_key = new_string[index] + " " + new_string[index + 1]
+            if dic_key in the_sequence:
+                the_sequence[dic_key].append(new_string[index + 2])
+                print("This is a duplicate item")
+            else:
+                the_sequence[dic_key] = [new_string[index + 2]]
+    for item in the_sequence:
+        print(item + " ---- " + str(the_sequence[item]))
 
-print(read_file('test_text.rtf'))
+#create_dict(my_string)
+create_dict(read_file('test_text.txt'))
