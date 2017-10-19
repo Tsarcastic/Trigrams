646d34e6e05f6b7c3f0b1518c83a7989f98b24f0
Brendan Davis <brendanmd@gmail.com>
2017-10-19 14:49:28 -0700

    can create the random text

--- trigrams.py
+++ trigrams.py
@@ -1,3 +1,4 @@
+import random
 my_string = """It was the best of times. It was the worst of times."""
 
 the_sequence = {}
@@ -18,11 +19,25 @@ def create_dict(file_name):
             dic_key = new_string[index] + " " + new_string[index + 1]
             if dic_key in the_sequence:
                 the_sequence[dic_key].append(new_string[index + 2])
-                print("This is a duplicate item")
+                #print("This is a duplicate item")
             else:
                 the_sequence[dic_key] = [new_string[index + 2]]
-    for item in the_sequence:
-        print(item + " ---- " + str(the_sequence[item]))
+    #for item in the_sequence:
+        #print(item + " ---- " + str(the_sequence[item]))
 
-#create_dict(my_string)
-create_dict(read_file('test_text.txt'))
+
+def write_madness(word1, word2, num_words=20):
+    first_words = word1 + " " + word2
+    paragraph = [word1, word2]
+    #print(paragraph[-2] + " " + paragraph[-1])
+    for x in range(num_words):
+        new = paragraph[-2] + " " + paragraph[-1]
+        to_be_added = random.choice(the_sequence[new])
+        paragraph.append(to_be_added)
+    print(paragraph)
+
+
+
+create_dict(my_string)
+#create_dict(read_file('test_text.txt'))
+write_madness("It", "was")
