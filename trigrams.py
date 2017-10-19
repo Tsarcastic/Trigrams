bf8ff18050cddd3ae9ffbc103b5fac7ddd98eb8a
Philip Werner <philip.r.werner@gmail.com>
2017-10-19 13:50:34 -0700

    Added function to open text to be read

--- trigrams.py
+++ trigrams.py
@@ -6,6 +6,13 @@ the_sequence = {}
 new_string = my_string.split()
 
 
+def read_file(text_file, result_file=None):
+    """This function will open the text file to be read."""
+    the_file = open(text_file, 'r')
+    data = the_file.read()
+    the_file.close()
+    return data
+
 for index, item in enumerate(new_string):
     if index < len(new_string) - 2:
         dic_key = new_string[index] + " " + new_string[index + 1]
@@ -16,3 +23,6 @@ for index, item in enumerate(new_string):
             the_sequence[dic_key] = [new_string[index + 2]]
 for item in the_sequence:
     print(item + " ---- " + str(the_sequence[item]))
+
+
+print(read_file('test_text.rtf'))
